2e47a5b5279dbfce50e56b617b3f8b732834ef49
rodolfodrago <rodolfo.drago@gmail.com>
2016-12-23 00:01:29 -0200

    Add project files.

---
+++ MoveShapeDemo/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(MoveShapeDemo.Startup))]
+
+namespace MoveShapeDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
